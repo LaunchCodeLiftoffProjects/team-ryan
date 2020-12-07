1e77b7976b04c238086bec5ff51c6df97a1a202f
sunitha <sunitha>
2020-12-07 16:34:20 -0600

    First Step

---
+++ src/main/java/org/launchcode/recipeapp/controllers/EventController.java
@@ -0,0 +1,16 @@
+package org.launchcode.recipeapp.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class EventController {
+    @RequestMapping(value = "hello",method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String name, @RequestParam String about, @RequestParam String ingr, @RequestParam String direc, @RequestParam String tips){
+        return name + "\n" + about + "\n" + ingr + "\n" + direc + "\n" + tips;
+    }
+    @GetMapping("addrecipe")
+    public String helloForm(){
+    return "addrecipe";
+    }
+}
