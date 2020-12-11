5a11e52a1c0ae6ef9e3b0041ddb91aed3bf0f989
Ryan Walker <rawn8c@mail.umsl.edu>
2020-12-10 20:34:29 -0600

    fixed routing errors. see changed files on github for more info.

--- src/main/java/org/launchcode/recipeapp/controllers/RecipeController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/RecipeController.java
@@ -3,14 +3,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("recipes")
 public class RecipeController {
-    @RequestMapping(value = "hello",method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public String hello(@RequestParam String name, @RequestParam String about, @RequestParam String ingr, @RequestParam String direc, @RequestParam String tips){
         return name + "\n" + about + "\n" + ingr + "\n" + direc + "\n" + tips;
     }
     @GetMapping("addrecipe")
     public String helloForm(){
-    return "addrecipe";
+    return "recipe/addrecipe";
     }
 }
