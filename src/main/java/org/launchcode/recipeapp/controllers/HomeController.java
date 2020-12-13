0fc3a06736ccf121b62844b3f6861f0b6b33182a
zoelzer220 <mmp6301@gmail.com>
2020-12-12 21:22:02 -0600

    initial branch commit

--- src/main/java/org/launchcode/recipeapp/controllers/HomeController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package org.launchcode.recipeapp.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping(value="", produces = "application/json")
-    public String index() {
-        return "{\"greeting\":\"hello world\"}";
+    public String index(Model model) {
+
+        return "index";
+//        return "{\"greeting\":\"hello world\"}";
     }
 }
