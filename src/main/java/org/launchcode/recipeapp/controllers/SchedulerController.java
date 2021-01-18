45086c3de2bd7422cd7441b2fb66722c06b0a812
zoelzer220 <mmp6301@gmail.com>
2021-01-18 11:12:58 -0600

    Committing work to update main.

--- src/main/java/org/launchcode/recipeapp/controllers/SchedulerController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/SchedulerController.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("")
-    public String index() {
-        return "landing";
-    }
-}
+public class SchedulerController {
 
+    @GetMapping("/schedule")
+    public String index() { return "scheduler"; }
+}
