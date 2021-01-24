9880455543a4894a87bef9bd17fea50d18ff56a7
zoelzer220 <mmp6301@gmail.com>
2021-01-23 23:23:48 -0600

    Drag/drop complete. Order problem remaining.

--- src/main/java/org/launchcode/recipeapp/controllers/SchedulerController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/SchedulerController.java
@@ -1,6 +1,7 @@
 package org.launchcode.recipeapp.controllers;
 
 import org.launchcode.recipeapp.data.IngredientRepository;
+import org.launchcode.recipeapp.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("schedule")
 public class SchedulerController {
 
+//    @Autowired
+//    private IngredientRepository ingredientRepository;
+//
+//    @GetMapping
+//    public String displayAllIngredients(Model model) {
+//        model.addAttribute("name", "All Ingredients");
+//        model.addAttribute("ingredients", ingredientRepository.findAll());
+//        return "scheduler";
+//    }
+
     @Autowired
-    private IngredientRepository ingredientRepository;
+    private RecipeRepository recipeRepository;
 
     @GetMapping
-    public String displayAllIngredients(Model model) {
-        model.addAttribute("name", "All Ingredients");
-        model.addAttribute("ingredients", ingredientRepository.findAll());
+    public String displayRecipes(Model model) {
+        model.addAttribute("name", "All Recipes");
+        model.addAttribute("recipes", recipeRepository.findAll());
         return "scheduler";
     }
 }
