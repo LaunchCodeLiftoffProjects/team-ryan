05571b5e669700c5c591d15402717e8a758e14a4
Katie Yarber <kyarber2@gmail.com>
2021-01-25 13:03:13 -0600

    pushing code again API is still calling on page load

--- src/main/java/org/launchcode/recipeapp/controllers/SearchController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/SearchController.java
@@ -1,16 +1,48 @@
 package org.launchcode.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class SearchController {
-    @GetMapping("search")
-    //@ResponseBody
-    public String index(){
-        return "search/index";
 
+//every path starts with recipes
+//@RequestMapping("recipes")
+public class SearchController {
+    //lives at /recipes/search
+    @GetMapping("search")
+    public String renderSearchIndex(){
+        //renders search/index
+        return "search/index";
     }
+//    lives at /recipes/search
+//    handles requests of the form /search + search term
+//    query param - id
+
+//    @GetMapping("search")
+//    //accepts both get and post requests, lives at recipes/search
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "search")
+//    public String searchWithQueryParam(@RequestParam String id) {
+//        return "Id is " + id;
+//    }
+
+    @GetMapping("recipes/search")
+    public String recipesWithQueryParam(@RequestParam String id ){
+        //displays text on the page
+        return "search/recipes" + id;
+    }
+
+    //handles requests of the form /recipes/search
+//    @GetMapping("search")
+//    public String searchForm(){
+//        return
+//
+//    }
+//
+
+//    @GetMapping("recipes/fromAPI")
+//    @ResponseBody
+//    public String grabAPIRecipeId(@RequestParam String id) {
+//        return "ID " + id;
+//    }
 
 }
