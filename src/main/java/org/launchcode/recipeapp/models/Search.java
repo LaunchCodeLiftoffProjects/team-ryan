f8ef552d6d7c8742d7dfcb1005eda0ec8e253781
Katie Yarber <kyarber2@gmail.com>
2021-01-25 16:40:21 -0600

    adding model for search

--- src/main/java/org/launchcode/recipeapp/models/Search.java
+++ src/main/java/org/launchcode/recipeapp/models/Search.java
@@ -1,2 +1,43 @@
-package org.launchcode.recipeapp.models;public class Search {
+package org.launchcode.recipeapp.models;
+
+import java.util.Objects;
+
+public class Search {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Search search = (Search) o;
+        return id == search.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //    private String title;
+//
+//    public Search(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return title;
+//    }
 }
